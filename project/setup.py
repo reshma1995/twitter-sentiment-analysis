4403518dd52f038b3663b926d410a0b24f6656a4
reshma1995 <reshmadsouza263@gmail.com>
2025-04-19 02:46:49 +0100

    ADD: Setup file

---
+++ project/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Twitter tweets data to classify the sentiment of different Tweets using Deep Learning methodologies',
+    author='Reshma Dsouza',
+    license='',
+)
